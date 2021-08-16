f1d67b91218cd699aa61febcc9072c0a3fe3a2de
nullchinchilla <nullchinchilla@pm.me>
2021-08-16 10:31:00 -0500

    fix

--- autosetup.bash
+++ autosetup.bash
@@ -9,14 +9,14 @@ set -e
 
 
 echo "STEP 1: Installing Rust / 正在安裝 Rust"
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+yes | curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 source $HOME/.cargo/env
 
 echo "STEP 2: Compiling geph4-exit / 正在編譯 geph4-exit"
 cargo install --locked geph4-exit
 
 echo "STEP 3: Creating config file / 創建設置文件"
-tee ~/geph4-exit.toml << EOF
+dd of=~/geph4-exit.toml << EOF
 # Where to listen for incoming connections. Change 8814 to whatever port you like
 sosistab_listen = "[::]:8814"
 # Where to store secret key
@@ -24,7 +24,7 @@ secret_key = "$HOME/geph4-exit.key"
 EOF
 
 echo "STEP 4: Creating systemd unit / 啓用 systemd 服務"
-sudo tee /etc/systemd/system/geph4-exit.service << EOF 
+sudo dd of=/etc/systemd/system/geph4-exit.service << EOF 
 [Unit]
 Description=Geph4 bridge service.
 [Service]
