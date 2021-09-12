db8da3f4a77e9086edc55375a62f1046ab829d16
nullchinchilla <nullchinchilla@pm.me>
2021-09-12 09:14:04 -0500

    lel

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cargo build --release --target x86_64-unknown-linux-musl --manifest-path ~/GEPH4/geph4-exit/Cargo.toml
+
+for host in us-pdx-01.exits.geph.io nl-ams-01.exits.geph.io sosistab-jp-test.labooyah.be sg-sgp-03.exits.geph.io ca-mtl-02.exits.geph.io tw-rmq-01.exits.geph.io          
+do                                         
+rsync -avz --progress ~/GEPH4/geph4-exit/target/x86_64-unknown-linux-musl/release/geph4-exit root@$host:/usr/local/bin/ &
+done; wait;
+
+for host in us-pdx-01.exits.geph.io sosistab-jp-test.labooyah.be sg-sgp-03.exits.geph.io tw-rmq-01.exits.geph.io nl-ams-01.exits.geph.io
+do                             
+ssh root@$host service geph4-exit restart &
+done; wait;
+
