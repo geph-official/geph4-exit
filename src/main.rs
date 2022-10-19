bb638288af5fc1c3b3bea7f5a7815d2bb75f2f68
nullchinchilla <nullchinchilla@pm.me>
2022-10-19 06:56:27 -0500

    disable mimalloc for testing

--- src/main.rs
+++ src/main.rs
@@ -30,8 +30,8 @@ struct Opt {
     /// Path to configuration file. Can be a HTTP URL!
     config: String,
 }
-#[global_allocator]
-static GLOBAL: MiMalloc = MiMalloc;
+// #[global_allocator]
+// static GLOBAL: MiMalloc = MiMalloc;
 
 fn main() -> anyhow::Result<()> {
     if std::env::var("GEPH_SINGLETHREADED").is_ok() {
