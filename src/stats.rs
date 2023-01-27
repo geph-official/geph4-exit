cbed0d0fbcf4881e9c213029f61d042cb4cbdee3
nullchinchilla <nullchinchilla@pm.me>
2023-01-26 20:17:21 -0600

    oh no

--- src/stats.rs
+++ src/stats.rs
@@ -23,7 +23,7 @@ impl<P: Pipe> StatsPipe<P> {
 #[async_trait]
 impl<P: Pipe> Pipe for StatsPipe<P> {
     async fn send(&self, to_send: Bytes) {
-        if fastrand::f64() > to_send.len() as f64 / 1_000_000.0 {
+        if fastrand::f64() < to_send.len() as f64 / 1_000_000.0 {
             self.statsd_client.count(&self.flow_key, 1_000_000.0);
         }
         self.inner.send(to_send).await;
@@ -31,7 +31,7 @@ impl<P: Pipe> Pipe for StatsPipe<P> {
 
     async fn recv(&self) -> std::io::Result<Bytes> {
         let recved = self.inner.recv().await?;
-        if fastrand::f64() > recved.len() as f64 / 1_000_000.0 {
+        if fastrand::f64() < recved.len() as f64 / 1_000_000.0 {
             self.statsd_client.count(&self.flow_key, 1_000_000.0);
         }
         Ok(recved)
